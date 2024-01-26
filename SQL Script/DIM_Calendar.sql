cf8c959efc7dcb91fa9f941d141a73c0815db50b
Firhan Imam Haekal <firhanimamhaekal@gmail.com>
2024-01-26 22:03:50 +0700

    Added Exported CSV and SQL Scripts

---
+++ SQL Script/DIM_Calendar.sql
@@ -0,0 +1,11 @@
+-- Cleaned DIM_DateTable --
+SELECT  [DateKey]
+       ,[FullDateAlternateKey] AS Date
+       ,[EnglishDayNameOfWeek] AS Day
+       ,[WeekNumberOfYear] AS WeekNo
+       ,[EnglishMonthName] AS Month
+       ,LEFT([EnglishMonthName, 3]) AS MonthShort
+       ,[MonthNumberOfYear] AS MonthNo
+       ,[CalendarQuarter] AS Quarter
+       ,[CalendarYear] AS Year
+FROM [AdventureWorksDW2022].[dbo].[DimDate]
