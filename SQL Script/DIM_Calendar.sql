8f3af26c10d31d0a8cfc4d7e854130f729cce6c6
Firhan Imam Haekal <firhanimamhaekal@gmail.com>
2024-01-28 10:31:30 +0700

    Completed Dashboard MVP

--- SQL Script/DIM_Calendar.sql
+++ SQL Script/DIM_Calendar.sql
@@ -4,8 +4,9 @@ SELECT  [DateKey]
        ,[EnglishDayNameOfWeek] AS Day
        ,[WeekNumberOfYear] AS WeekNo
        ,[EnglishMonthName] AS Month
-       ,LEFT([EnglishMonthName, 3]) AS MonthShort
+       ,LEFT([EnglishMonthName],3) AS MonthShort
        ,[MonthNumberOfYear] AS MonthNo
        ,[CalendarQuarter] AS Quarter
        ,[CalendarYear] AS Year
 FROM [AdventureWorksDW2022].[dbo].[DimDate]
+WHERE CalendarYear >= 2022
